f5a6472813b01284cfe89a8089d6f984da7fa17b
Simple <xoeilnsl@gmail.com>
2024-05-27 19:55:21 +0100

    Update test.js

--- main/test.js
+++ main/test.js
@@ -1,16 +1,1 @@
-const modal = document.getElementById("myModal");
-const cancelButton = document.getElementById("cancelButton");
-const deleteButton = document.getElementById("deleteButton");
 
-// Show the modal
-modal.style.display = "block";
-
-cancelButton.onclick = function() {
-  modal.style.display = "none";
-  window.location.href = "http://your-cancel-link.com";
-}
-
-deleteButton.onclick = function() {
-  modal.style.display = "none";
-  window.location.href = "http://your-delete-link.com";
-}
