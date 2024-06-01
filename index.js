8e73e0bc3fc181f12d89043687e95a794acd681c
Simple <xoeilnsl@gmail.com>
2024-06-01 03:13:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 
+document.getElementById('postButton').addEventListener('click', function() {
+    const appName = document.getElementById('appName').value;
+    const appCategory = document.getElementById('appCategory').value;
+    const appLink = document.getElementById('appLink').value;
+
+    if (appName && appCategory && appLink) {
+        const newPost = document.createElement('div');
+        newPost.classList.add('app-prompt');
+        newPost.innerHTML = `
+            <div class="app-info">
+                <span class="new-label">New</span>
+                <h3>${appName}</h3>
+                <p>${appCategory}</p>
+                <a href="${appLink}" target="_blank">Install</a>
+            </div>
+        `;
+
+        document.getElementById('appPromptsContainer').prepend(newPost);
+
+        // Remove "New" label after 7 days
+        setTimeout(() => {
+            newPost.querySelector('.new-label').remove();
+        }, 7 * 24 * 60 * 60 * 1000); // 7 days in milliseconds
+    } else {
+        alert('Please fill in all fields.');
+    }
+});
